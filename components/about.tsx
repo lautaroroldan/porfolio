a22172f4912085d81fc8b7f57c02412d3d360218
Lautaro Roldan <lautaronicolasroldan@gmail.com>
2024-11-12 21:01:31 -0300

    Fix eslint

--- components/about.tsx
+++ components/about.tsx
@@ -6,10 +6,10 @@ function About() {
         <Section>
             <article className='prose-base text-custom-gray prose-p:text-base prose-p:leading-normal w-full'>
                 <p>
-                    Hello! <span className='text-white font-bold'>I'm Lautaro Roldan</span>, a fullstack web developer from Argentina with over 2 years of experience. Currently, I’m focusing on frontend development within the React ecosystem and freelancing on diverse projects.
+                    Hello! <span className='text-white font-bold'>I&apos;m Lautaro Roldan</span>, a fullstack web developer from Argentina with over 2 years of experience. Currently, I&apos;m focusing on frontend development within the React ecosystem and freelancing on diverse projects.
                 </p>
                 <p>
-                    I’m passionate about staying up-to-date with emerging tools and projects in the tech world. Outside of coding, I have a strong interest in music and am learning bass guitar along with other instruments.
+                    I&apos;m passionate about staying up-to-date with emerging tools and projects in the tech world. Outside of coding, I have a strong interest in music and am learning bass guitar along with other instruments.
                 </p>
             </article>
         </Section>
